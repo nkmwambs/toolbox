9e6dbe9b48b5cd6e9b31dae6de186eadc378c0b5
root <root@server.compassionkenya.com>
2019-06-25 17:39:49 +0300

    Removed ignored files

--- system/shared/config/general.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['system_name'] = "Compassion Toolkit";
-
--- system/shared/config/techsys.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-
-$config['system_name']="CRM";
-
-$config['encryption_key'] = 'super-secret-key';
-
-?>
