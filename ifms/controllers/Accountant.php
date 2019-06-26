be89843655247a7f3cfdc5612a927c44ca07b131
Nicodemus Karisa <nkarisa@ke.ci.org>
2019-06-26 10:58:00 +0300

    Started working on Cron controller - clear_cached_data

--- ifms/controllers/Accountant.php
+++ ifms/controllers/Accountant.php
@@ -2,14 +2,6 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-/*	
- *	@author 	: Joyonto Roy
- *	date		: 27 september, 2014
- *	FPS School Management System Pro
- *	http://codecanyon.net/user/FreePhpSoftwares
- *	support@freephpsoftwares.com
- */
-
 class Accountant extends CI_Controller
 {
     
---
+++ ifms/controllers/Crons.php
@@ -0,0 +1,39 @@
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+class Crons extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		$this -> load -> database();
+		$this -> load -> library('session');
+	}
+	
+	
+	private function clear_cached_data(){
+		$this->db->cache_delete_all();
+	}
+	
+	
+	public function minute_jobs(){
+		$this->clear_cached_data();
+	}
+	
+	public function semi_hourly_jobs(){
+		
+	}
+	
+	public function hourly_jobs(){
+		
+	}
+	
+	public function daily_jobs(){
+		
+	}
+	
+	public function weekly_jobs(){
+		
+	}
+
+}
