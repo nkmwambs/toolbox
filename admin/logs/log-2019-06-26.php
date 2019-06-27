3083267ac62b4aa75412dbcfecb1f10832bc8821
Onduso <londuso@us.ci.org>
2019-06-27 16:30:18 +0300

    disallow direct access

---
+++ admin/logs/log-2019-06-26.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-06-26 17:02:22 --> Query error: Table 'compatl8_mvc.visited_notifications' doesn't exist - Invalid query: SELECT *
+FROM `visited_notifications`
+WHERE `link_id` = '2'
+AND `user_id` = '3355'
+ERROR - 2019-06-26 17:03:49 --> Query error: Table 'compatl8_mvc.visited_notifications' doesn't exist - Invalid query: SELECT *
+FROM `visited_notifications`
+WHERE `link_id` = '2'
+AND `user_id` = '3355'
+ERROR - 2019-06-26 17:05:05 --> Query error: Table 'compatl8_mvc.visited_notifications' doesn't exist - Invalid query: SELECT *
+FROM `visited_notifications`
+WHERE `link_id` = '2'
+AND `user_id` = '3355'
